5bc39e47825cd912688e229a8222eaba5d11317f
alitan2014 <alitan2014.163.com>
2019-04-23 18:03:02 +0800

    视频

---
+++ VideoEditDemo/CLVideoRecordingManager/CLCompressUtil.h
@@ -0,0 +1,17 @@
+//
+//  CLCompressUtil.h
+//  VideoEditDemo
+//
+//  Created by 谭春林 on 2019/4/22.
+//  Copyright © 2019 谭春林. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CLCompressUtil : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ VideoEditDemo/CLVideoRecordingManager/CLCompressUtil.m
@@ -0,0 +1,13 @@
+//
+//  CLCompressUtil.m
+//  VideoEditDemo
+//
+//  Created by 谭春林 on 2019/4/22.
+//  Copyright © 2019 谭春林. All rights reserved.
+//
+
+#import "CLCompressUtil.h"
+
+@implementation CLCompressUtil
+
+@end
